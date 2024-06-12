11bbe9a6e25ca8560ffdb4243c0308302e5537c9
polya001 <polya001@mail.ru>
2024-06-12 23:33:20 +0300

    add code

---
+++ calc_functions.py
@@ -0,0 +1,22 @@
+
+"""
+Module with basic calc functions
+"""
+
+def add(x, y):
+    """Addition function"""
+    return x + y
+
+def subtract(x, y):
+    """Subtraction function"""
+    return x - y
+
+def multiply(x, y):
+    """Multiplication function"""
+    return x * y
+
+def divide(x, y):
+    """Division function"""
+    if y == 0:
+        return "ERROR"
+    return x / y
